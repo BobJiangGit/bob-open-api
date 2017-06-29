9e24548432af06721f41d8eeb6c340329f5aeebf
Bob Jiang <jw244849875@gmail.com>
2017-06-29 17:28:53 +0800

    home page redirect

--- src/main/java/org/bob/open/api/controller/HomeController.java
+++ src/main/java/org/bob/open/api/controller/HomeController.java
@@ -3,12 +3,18 @@ package org.bob.open.api.controller;
 import org.bob.open.api.dto.response.ResultResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
 public class HomeController {
 
     @RequestMapping(value = "/")
+    public ModelAndView root() {
+        return new ModelAndView("redirect:/open-api");
+    }
+
+    @RequestMapping(value = "/open-api")
     public ResultResponse<String> home() {
         return ResultResponse.success("welcome openAPI.");
     }
